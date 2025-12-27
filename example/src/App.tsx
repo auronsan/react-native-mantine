edc57207f3a84bb80df7717a350c6d4fd13b5035
auronsan <auronsanjr@gmail.com>
2025-12-28 02:15:25 +0700

    feat: update docs

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -13,7 +13,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <Theme>
+    <Theme forceMode="light">
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Home"
